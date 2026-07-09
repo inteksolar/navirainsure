769850dc32c82f37aea67b6f9d8a3204ae520f0c
inteksolar <inteksolar@gmail.com>
2026-07-09 09:48:12 +0530

    Implement mobile menu toggle functionality

---
+++ js/about.js
@@ -0,0 +1,92 @@
+/* ========================================
+   MOBILE MENU
+======================================== */
+
+const menuBtn =
+  document.getElementById("menuBtn");
+
+
+const navLinks =
+  document.getElementById("navLinks");
+
+
+
+menuBtn.addEventListener(
+  "click",
+  function () {
+
+
+    navLinks.classList.toggle("show");
+
+
+    const icon =
+      menuBtn.querySelector("i");
+
+
+    if (
+      navLinks.classList.contains("show")
+    ) {
+
+      icon.classList.remove(
+        "fa-bars"
+      );
+
+      icon.classList.add(
+        "fa-xmark"
+      );
+
+    }
+
+    else {
+
+      icon.classList.remove(
+        "fa-xmark"
+      );
+
+      icon.classList.add(
+        "fa-bars"
+      );
+
+    }
+
+  }
+);
+
+
+
+/* ========================================
+   CLOSE MENU AFTER LINK CLICK
+======================================== */
+
+document
+  .querySelectorAll(".nav-links a")
+  .forEach(function (link) {
+
+
+    link.addEventListener(
+      "click",
+      function () {
+
+
+        navLinks.classList.remove(
+          "show"
+        );
+
+
+        const icon =
+          menuBtn.querySelector("i");
+
+
+        icon.classList.remove(
+          "fa-xmark"
+        );
+
+
+        icon.classList.add(
+          "fa-bars"
+        );
+
+      }
+    );
+
+  });
